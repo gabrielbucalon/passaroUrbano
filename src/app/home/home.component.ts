616d4c524a75670a33e25505be764a21adfc63b6
Gabriel <bucalongabriell@gmail.com>
2018-10-09 14:49:21 -0300

    Ajuste e adição de novos serviços

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../../shared/oferta.model';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+  constructor(private ofertasService: OfertasService) {}
 
-  constructor() { }
+  public ofertas: Oferta[];
 
   ngOnInit() {
+    // tslint:disable-next-line:prefer-const
+    this.ofertas = this.ofertasService.getOfertas();
   }
-
 }
